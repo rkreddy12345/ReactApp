983512d487a69685020796e65f458ac14ece6ba4
Radha Krishna <rkanugonda969@gmail.com>
2025-06-11 22:54:26 +0530

    hello JSX

--- index.js
+++ index.js
@@ -6,26 +6,7 @@ import ReactDOM from "react-dom/client";
  * for prod build: npx parcel build index.html
  */
 
-const heading1 = React.createElement(
-    "h1",
-    {
-      id: "title1",
-    },
-    "heading1"
-  );
-  const heading2 = React.createElement(
-    "h2",
-    {
-      id: "title2",
-    },
-    "heading2"
-  );
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [heading1, heading2]
-  );
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(container);
+const heading1 = <h1>Namaste everyone!</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading1);
