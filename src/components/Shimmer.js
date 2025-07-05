aa214e25287b83470083fe55a41cd6aba8c91ead
Radha Krishna <rkanugonda969@gmail.com>
2025-07-05 22:37:48 +0530

    better shimmer cards

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,5 +1,12 @@
 const Shimmer = () => {
-    return <h1>Loading...</h1>;
+    return <div className="restaurant-list">
+        {Array(16).fill("").map(
+            //index is bad key, just for quick start
+            (e, index) => <div key={index} className="shimmer-card"></div>
+        )}
+
+
+    </div>
 }
 
 export default Shimmer;
